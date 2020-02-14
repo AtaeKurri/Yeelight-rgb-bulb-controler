66c91ba9561aea860ff077f73e9feef2980b2849
AtaeKurri <60986988+AtaeKurri@users.noreply.github.com>
2020-02-14 10:34:20 +0100

    Update app.py
    Added some additional support for custom color without the need of rgb codes (like blue, green, red and magenta)
    Added support for errors and make the program go to main() if an error happened

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 # Python script for using yeelight rgb bulb.
 # Made by Atae Kurri
 
-#Version 1.0.0
+#Version 1.0.1
 
 from yeelight import Bulb
 import json, jsonpickle
 
-with open("config.json", 'r') as config1:
-    data1 = json.load(config1)
-
 def main():
+    with open("config.json", 'r') as config1:
+        data1 = json.load(config1)
     try:
         commande = input("(help) > ")
         commande = commande.split(" ")
@@ -26,6 +25,29 @@ def main():
                         main()
             except Exception as error:
                 print(f"[{error}]")
+        elif commande[0] == "help":
+            print("Please verify the spelling of the command.")
+            print("""
+- <bulb name> turn on/off
+- <bulb name> pre <see config.json>
+- <bulb name> brightness int
+- <bulb name> rgb int int int
+- addbulb <name> <ip>
+- reloadconf""")
+            main()
+        elif commande[0] == "reloadconf":
+            confirmation = input("Are you sure you want to reset your configuration file ? (Y/N) > ")
+            if confirmation == "Y":
+                data1 = {}
+                data1["ips"] = {}
+                data1["prereglage"] = {}
+                data1["colors"] = {}
+                with open("config.json", 'w') as config:
+                    json.dump(data1, config, indent=2)
+                print("Done.")
+                main()
+            else:
+                main()
         elif commande[0] in data1["ips"]:
             bulb = Bulb(data1["ips"][commande[0]])
             if commande[1] == "turn":
@@ -55,19 +77,31 @@ def main():
                 bulb.set_rgb(data1["prereglage"][option]["rgb1"], data1["prereglage"][option]["rgb2"], data1["prereglage"][option]["rgb3"])
                 print("Done")
                 main()
+            elif commande[1] == "color":
+                option = commande[2]
+                rgb = data1["colors"][option].split(",")
+                print(f"{rgb[0]},{rgb[1]},{rgb[2]}")
+                bulb.set_rgb(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+                print("Done")
+                main()
             else:
                 print("Please verify the spelling of the command.")
                 print("""
-    - <bulb name> turn on/off
-    - <bulb name> pre <see config.json>
-    - <bulb name> brightness int
-    - <bulb name> rgb int int int
-    - addbulb <name> <ip>""")
+- <bulb name> turn on/off
+- <bulb name> pre <see config.json>
+- <bulb name> brightness int
+- <bulb name> rgb int int int
+- addbulb <name> <ip>
+- reloadconf""")
                 main()
+        elif not commande[0]:
+            print("Please type 'help' to see the list of commands.")
+            main()
         else:
             print(f"You have no bulb named {commande[0]}")
-    except:
-        print("The connection to your bulb has been reseted. Please restart the program.")
+            main()
+    except Exception as error:
+        print(f"The connection to your bulb has been reseted. Please restart the program. {error}")
 
 
 if __name__ == '__main__':
